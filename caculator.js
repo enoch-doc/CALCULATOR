6d65509dbd8ecc93fd4a97776d4cecda593f8bf8
Enoch Aremu <110859297+enoch-doc@users.noreply.github.com>
2022-09-01 00:35:54 +0100

    I updated the javascript file for the calculator

---
+++ caculator.js
@@ -0,0 +1,202 @@
+function darkmode() {
+    document.getElementById("inner").style.backgroundColor = "rgb(210, 216, 210)";
+    document.getElementById("screen").style.backgroundColor = "white";
+    document.getElementById("time").style.backgroundColor = "white";
+    document.getElementById("time").style.color = "black";
+    document.getElementById("display").style.color = "black";
+    document.getElementById("cac").style.color = "black";
+    document.getElementById("cac").style.backgroundColor = "white";
+    document.getElementById("current").style.color = "black";
+var buttons = document.getElementsByTagName('button').item(1).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(2).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(3).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(4).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(6).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(7).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(8).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(9).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(11).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(12).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(13).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(14).style.color = "black"
+var buttons = document.getElementsByTagName('button').item(1).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(2).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(3).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(4).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(5).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(6).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(7).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(8).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(9).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(10).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(11).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(12).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(13).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(14).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(15).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(16).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(17).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(18).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(19).style.backgroundColor = "white"
+var buttons = document.getElementsByTagName('button').item(20).style.backgroundColor = "white"
+}
+function lightmode() {
+    
+    document.getElementsByTagName('div').item(1).style.backgroundColor = "black"
+    document.getElementById("inner").style.backgroundColor = "rgb(78, 73, 73)";
+    document.getElementById("time").style.backgroundColor = "black";
+    document.getElementById("time").style.color = "white";
+    document.getElementById("display").style.color = "white";
+    document.getElementById("cac").style.color = "white";
+    document.getElementById("cac").style.backgroundColor = "black";
+    document.getElementById("current").style.color = "white";
+    var buttons = document.getElementsByTagName('button').item(1).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(2).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(3).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(4).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(5).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(6).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(7).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(8).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(9).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(11).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(12).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(13).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(14).style.color = "white"
+var buttons = document.getElementsByTagName('button').item(1).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(2).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(3).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(4).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(5).style.backgroundColor = "rgba(175, 168, 168, 0.959)"
+var buttons = document.getElementsByTagName('button').item(6).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(7).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(8).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(9).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(10).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(11).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(12).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(13).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(14).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(15).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(16).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(17).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(18).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(19).style.backgroundColor = "black"
+var buttons = document.getElementsByTagName('button').item(20).style.backgroundColor = "black"
+   
+}
+
+function number1() {
+    cac.value += "1"
+    document.getElementById('current').innerText = ""
+}
+function number2() {
+    cac.value += "2"
+    document.getElementById('current').innerText = ""
+}
+function number3() {
+    cac.value += "3"
+    document.getElementById('current').innerText = ""
+}
+function number4() {
+    cac.value += "4"
+    document.getElementById('current').innerText = ""
+}
+function number5() {
+    cac.value += "5"
+    document.getElementById('current').innerText = ""
+}
+function number6() {
+    cac.value += "6"
+    document.getElementById('current').innerText = ""
+}
+function number7() {
+    cac.value += "7"
+    document.getElementById('current').innerText = ""
+}
+function number8() {
+    cac.value += "8"
+    document.getElementById('current').innerText = ""
+}
+function number9() {
+    cac.value += "9"
+    document.getElementById('current').innerText = ""
+}
+function number0() {
+     cac.value += "0"
+     document.getElementById('current').innerText = ""
+}
+function numberp() {
+    cac.value += "/100"
+    document.getElementById('current').innerText = ""
+}
+function numberdec() {
+    cac.value += "."
+    document.getElementById('current').innerText = ""
+}
+function operatord() {
+    cac.value += "/"
+    document.getElementById('current').innerText = ""
+}
+function operatora() {
+    cac.value += "+"
+    document.getElementById('current').innerText = ""
+}
+function operatorm() {
+    cac.value += "*"
+    document.getElementById('current').innerText = ""
+}
+function operators() {
+    cac.value += "-"
+    document.getElementById('current').innerText = ""
+}
+function del() {
+    cac.value = cac.value.slice(0, -1)
+}
+function sqrt() {
+    var inp = cac.value
+    var outp = Math.pow(inp, 2)
+    if(outp = outp) {
+        cac.value = inp + '²'
+        document.getElementById('current').innerText = "=" + outp
+        cac.value = ""
+    } else if ( document.getElementById('current').innerText = "=" + "Nan") {
+        cac.value = inp + '²'
+        document.getElementById('current').innerText = "Syntax Error" 
+        cac.value = ""
+    }
+}
+function display() {
+    var input = cac.value
+    var output = eval(input)
+    document.getElementById('current').innerText = "=" + output
+    if ( output =  output) {
+        document.getElementById('current').innerText = "=" + output 
+        cac.value = ""
+    } else if(document.getElementById('current').       innerText = "=" + "Nan"){
+        document.getElementById('current').innerText = "Syntax Error" 
+        cac.value = ""
+    }
+}
+function clr() {
+    cac.value = " "
+    document.getElementById('current').innerText=""
+}
+var timer = setInterval(show, 1000)
+function show() {
+    let date = new Date()
+    let hr = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    // hr = checkTime(hr)
+    min = checkTime(min)
+    sec = checkTime(sec)
+    secs.innerHTML = sec
+    hour.innerHTML = `${hr} ${min}`
+}
+function checkTime(i) {
+    if (i<10) {
+        i = "0" +  i
+    }
+    return i
+}
